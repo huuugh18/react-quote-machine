bd267ecec88ca98f2d3a8f4409901a7070491d2d
Hugh O'Neill <hugh.oneill.4@gmail.com>
2019-02-19 09:33:40 -0700

    Updates

--- precache-manifest.656be44defa8f54a5d8a46d39f131534.js
+++ precache-manifest.656be44defa8f54a5d8a46d39f131534.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react-quote-machine/static/js/runtime~main.3a59924a.js"
   },
   {
-    "revision": "939140e13760791e899c",
-    "url": "/react-quote-machine/static/js/main.939140e1.chunk.js"
+    "revision": "b63be6c676c7e8f1c33d",
+    "url": "/react-quote-machine/static/js/main.b63be6c6.chunk.js"
   },
   {
     "revision": "8af079e08fa62ad1443d",
     "url": "/react-quote-machine/static/js/1.8af079e0.chunk.js"
   },
   {
-    "revision": "939140e13760791e899c",
+    "revision": "b63be6c676c7e8f1c33d",
     "url": "/react-quote-machine/static/css/main.693f2e52.chunk.css"
   },
   {
-    "revision": "1a8dca39a10bf808caefb32492ae8cfc",
+    "revision": "efc1ed0a0c3022009acc3be7dab2e430",
     "url": "/react-quote-machine/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-quote-machine/precache-manifest.7082103d15d161a577641a605bdf5078.js"
+  "/react-quote-machine/precache-manifest.656be44defa8f54a5d8a46d39f131534.js"
 );
 
 workbox.clientsClaim();
