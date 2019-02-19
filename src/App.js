9862481c3f130dea524c63e4439262a7d5dfed75
Hugh O'Neill <hugh.oneill.4@gmail.com>
2019-02-19 08:44:05 -0700

    build base app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state={
+        quotes:[],
+        currentQuote:{
+          quote:'something',
+          author:'homer j'
+        }
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+  componentDidMount(){
+    fetch('https://aitorp6.herokuapp.com/quotes/api/quote/Homer%20J.%20Simpson')
+    .then(results => {
+        return results.json();
+    }).then(data => {
+        return this.setState({quotes:data.quotes})
+    }).then(() => {
+      let randomNum = Math.floor(Math.random() * 211)
+      let randomQuote = this.state.quotes[randomNum]
+      return this.setState({currentQuote:randomQuote})
+    })
+  }
+  handleClick () {
+    let randomSelection = Math.floor(Math.random() * 211);
+    this.setState((state) => ({
+      currentQuote: state.quotes[randomSelection]
+    }))
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <img src={'https://s-media-cache-ak0.pinimg.com/236x/33/58/18/335818b563208954ab8fc8896d2d3a18.jpg'} className="App-logo" alt="logo" />
+          <Card id="quote-box">
+            <CardActionArea>
+                <CardContent>
+                  <Typography component="h1" id="text">
+                    "{this.state.currentQuote.quote ? this.state.currentQuote.quote : 'placeholder'}"
+                  </Typography>
+                  <Typography variant="h5" component="h2" id="author">
+                    - {this.state.currentQuote.author ? this.state.currentQuote.author : 'some author'}
+                  </Typography>
+
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size="small" color="primary" id="new-quote" onClick={this.handleClick}>
+                  New Quote
+                </Button>
+                <Button size="small" color="primary"  href="twitter.com/intent/tweet" id="tweet-quote">
+                  Tweet
+                </Button>
+              </CardActions>
+            </Card>
       </div>
     );
   }
