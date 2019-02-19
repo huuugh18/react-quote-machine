e9d44bc4c1ecd3273437834db73c6f7376fff669
Hugh O'Neill <hugh.oneill.4@gmail.com>
2019-02-19 09:17:21 -0700

    Updates

---
+++ precache-manifest.7082103d15d161a577641a605bdf5078.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a59924a8910b7f85b72",
+    "url": "/react-quote-machine/static/js/runtime~main.3a59924a.js"
+  },
+  {
+    "revision": "939140e13760791e899c",
+    "url": "/react-quote-machine/static/js/main.939140e1.chunk.js"
+  },
+  {
+    "revision": "8af079e08fa62ad1443d",
+    "url": "/react-quote-machine/static/js/1.8af079e0.chunk.js"
+  },
+  {
+    "revision": "939140e13760791e899c",
+    "url": "/react-quote-machine/static/css/main.693f2e52.chunk.css"
+  },
+  {
+    "revision": "1a8dca39a10bf808caefb32492ae8cfc",
+    "url": "/react-quote-machine/index.html"
+  }
+];
